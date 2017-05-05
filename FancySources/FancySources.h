5bae5907748c36f3f9be4d62c3029c1144c6bc27
Nikolay Ischuk <ischuknn@gmail.com>
2017-05-05 12:49:09 +0300

    Initial Commit

---
+++ FancySources/FancySources.h
@@ -0,0 +1,19 @@
+//
+//  FancySources.h
+//  FancySources
+//
+//  Created by Nikolay Ischuk on 05.05.17.
+//  Copyright © 2017 easyverzilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FancySources.
+FOUNDATION_EXPORT double FancySourcesVersionNumber;
+
+//! Project version string for FancySources.
+FOUNDATION_EXPORT const unsigned char FancySourcesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FancySources/PublicHeader.h>
+
+
